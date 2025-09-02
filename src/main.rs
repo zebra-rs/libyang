996d946817e51acce2be9e292572af3f2fd7ba9b
Kunihiro Ishiguro <kunihiro@zebra.dev>
2025-09-01 22:07:15 -0700

    Update version to 0.8.1.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ struct ErrorReporter;
 impl Report for ErrorReporter {}
 
 #[allow(dead_code)]
-fn main() -> Result<()> {
+fn main_test_action() -> Result<()> {
     env_logger::init();
     debug!("env logger started");
 
@@ -54,6 +54,35 @@ fn main2() -> Result<()> {
         }
         Err(e) => {
             return ErrorReporter::report_error(&e, file_name);
+        }
+    }
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn main() -> Result<()> {
+    env_logger::init();
+    debug!("env logger started");
+
+    let mut args: Vec<String> = env::args().collect();
+    while let Some(file_name) = args.pop() {
+        if args.is_empty() {
+            return Ok(());
+        }
+        println!("path {file_name}");
+        let input = fs::read_to_string(file_name.clone())
+            .with_context(|| format!("Can't read file {}", file_name))?;
+        let mut yang_grammar = YangGrammar::new();
+        let now = Instant::now();
+        match parse(&input, &file_name, &mut yang_grammar) {
+            Ok(_) => {
+                let elapsed_time = now.elapsed();
+                println!("Parsing took {} milliseconds.", elapsed_time.as_millis());
+                // println!("Success!\n{}", yang_grammar);
+            }
+            Err(e) => {
+                return ErrorReporter::report_error(&e, file_name);
+            }
         }
     }
     Ok(())
